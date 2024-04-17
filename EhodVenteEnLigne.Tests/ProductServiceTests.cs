eee67ccb4362e5d949211c82afbdfd946b5bb75f
saliousambadiao <elsaliousambadiao@gmail.com>
2024-04-17 18:32:31 +0000

    final commit

--- EhodVenteEnLigne.Tests/ProductServiceTests.cs
+++ EhodVenteEnLigne.Tests/ProductServiceTests.cs
@@ -1,30 +1,52 @@
-﻿using EhodBoutiqueEnLigne.Models.ViewModels;
+﻿using EhodBoutiqueEnLigne.Models;
+using EhodBoutiqueEnLigne.Models.Repositories;
+using EhodBoutiqueEnLigne.Models.Services;
+using EhodBoutiqueEnLigne.Models.ViewModels;
+using Microsoft.Extensions.Localization;
+using Moq;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using Xunit;
 
 namespace EhodBoutiqueEnLigne.Tests
-    {
+{
     public class ProductServiceTests
+    {
+        private readonly Mock<ICart> _mockCart;
+        private readonly Mock<IProductRepository> _mockProductRepository;
+        private readonly Mock<IOrderRepository> _mockOrderRepository;
+        private readonly Mock<IStringLocalizer<ProductService>> _mockLocalizer;
+        private readonly ProductService _productService;
+
+        public ProductServiceTests()
         {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
+            _mockCart = new Mock<ICart>();
+            _mockProductRepository = new Mock<IProductRepository>();
+            _mockOrderRepository = new Mock<IOrderRepository>();
+            _mockLocalizer = new Mock<IStringLocalizer<ProductService>>();
+            _mockLocalizer.Setup(l => l["MissingName"]).Returns(new LocalizedString("MissingName", "MissingName"));
+
+            _productService = new ProductService(_mockCart.Object, _mockProductRepository.Object, _mockOrderRepository.Object, _mockLocalizer.Object);
+        }
+
         [Fact]
-        public void ExampleMethod()
-            {
+        public void SaveProduct_MissingName_ShouldReturnErrorMessage()
+        {
             // Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Name = " ",
+                Description = "Test Description",
+                Price = "10,99",
+                Stock = "5"
+            };
 
             // Act
-
+            _productService.SaveProduct(productViewModel);
+            var modelErrors = _productService.CheckProductModelErrors(productViewModel);
 
             // Assert
-            Assert.Equal(1, 1);
-            }
-
-        // TODO write test methods to ensure a correct coverage of all possibilities
+            Assert.Contains("MissingName", modelErrors);
         }
 
     }
+}
